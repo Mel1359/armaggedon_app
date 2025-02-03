5dd42d7450400303d98b8e1069874cab1ea42907
Mel1359 <osttm888@gmail.com>
2025-02-03 23:38:50 +0300

    Updates

--- armaggedon-app/src/components/Bottom/Bottom.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Bottom.module.css';
-
-export const Bottom = () => {
-    return (
-        <div className={styles.bottom}>
-            2021 © Все права и планета защищены
-        </div>
-    );
-};
